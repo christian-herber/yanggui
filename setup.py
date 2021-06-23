7a54bd99299d95c09bf1766db85bd24b8be08d36
Christian Herber <christian.hrbr@gmail.com>
2021-06-23 19:21:30 +0200

    Bumping the package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    version='0.1.2',
+    version='0.1.3',
     name = "yanggui",
     python_requires=">=3.6",
     install_requires=[
